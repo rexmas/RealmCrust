9bbb48dc5cf103c1a2e56df04946fe5621a8b980
rexmas <rex.fenley@gmail.com>
2015-12-31 18:50:35 -0800

    [master] created RealmCrust xcodeproj

---
+++ RealmCrust/RealmCrust.h
@@ -0,0 +1,19 @@
+//
+//  RealmCrust.h
+//  RealmCrust
+//
+//  Created by Rex Fenley on 12/31/15.
+//  Copyright © 2015 RexDK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmCrust.
+FOUNDATION_EXPORT double RealmCrustVersionNumber;
+
+//! Project version string for RealmCrust.
+FOUNDATION_EXPORT const unsigned char RealmCrustVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmCrust/PublicHeader.h>
+
+
