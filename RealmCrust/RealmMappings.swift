da6ef5264f4f75a1268ca3eb271b2ee19860445e
rexmas <rex.fenley@gmail.com>
2016-02-13 14:55:41 -0800

    [primary-key-fix] Fixed crashing when using a primary key. Added a local cache for mapped objects.

--- RealmCrust/RealmMappings.swift
+++ RealmCrust/RealmMappings.swift
@@ -4,12 +4,14 @@ import RealmSwift
 public class RealmAdaptor {
     
     public typealias BaseType = Object
-    public typealias ResultsType = Results<Object>
+    public typealias ResultsType = Set<BaseType>
     
     var realm: Realm
+    var cache: NSMutableSet
     
     public init(realm: Realm) {
         self.realm = realm
+        self.cache = []
     }
     
     public convenience init() throws {
@@ -22,22 +24,26 @@ public class RealmAdaptor {
     
     public func mappingEnded() throws {
         try self.realm.commitWrite()
+        self.cache.removeAllObjects()
     }
     
     public func mappingErrored(error: ErrorType) {
         if self.realm.inWriteTransaction {
             self.realm.cancelWrite()
         }
+        self.cache.removeAllObjects()
     }
     
     public func createObject(objType: BaseType.Type) throws -> BaseType {
         let obj = objType.init()
+        self.cache.addObject(obj)
         return obj
     }
     
     public func saveObjects(objects: [BaseType]) throws {
         let saveBlock = {
             for obj in objects {
+                self.cache.removeObject(obj)
                 self.realm.add(objects, update: obj.dynamicType.primaryKey() != nil)
             }
         }
@@ -50,6 +56,7 @@ public class RealmAdaptor {
     
     public func deleteObject(obj: BaseType) throws {
         let deleteBlock = {
+            self.cache.removeObject(obj)
             self.realm.delete(obj)
         }
         if self.realm.inWriteTransaction {
@@ -74,12 +81,17 @@ public class RealmAdaptor {
     
     public func fetchObjectsWithType(type: BaseType.Type, predicate: NSPredicate) -> ResultsType? {
         
+        let objects = self.cache.filteredSetUsingPredicate(predicate)
+        if objects.count > 0 {
+            return objects as? ResultsType
+        }
+        
         if type.primaryKey() != nil {
             // We're going to build an unstored object and update when saving based on the primary key.
             return nil
         }
         
-        return realm.objects(type).filter(predicate)
+        return Set(realm.objects(type).filter(predicate))
     }
 }
 
